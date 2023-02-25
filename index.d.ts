10843d90704c40aa6d7b61bb3b2ae5657f7dd72f
kale <42823429+oelin@users.noreply.github.com>
2023-02-25 15:25:16 +0000

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'midjourney-client' {
+  interface MidjourneyOptions {
+    [key: string]: any;
+  }
+  export default function midjourney(prompt: string, inputs?: MidjourneyOptions): Promise<any>;
+}
