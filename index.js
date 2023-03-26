6fc47be98df5850e3d3994fe6f271d22c41aa557
kale <42823429+oelin@users.noreply.github.com>
2023-03-26 19:03:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import replicate from 'node-replicate'
 
-const model = replicate.model(
-	'prompthero/openjourney:9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb'
-)
+const model = 'prompthero/openjourney:9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb'
 
-export default async (prompt, parameters, onUpdate) => 
-	await model.predict({ prompt, ...parameters }, { onUpdate })
-		.then(prediction => prediction.output)
+export default async (prompt, parameters) => 
+	await replicate.run(model, { prompt, ...parameters })
+		.then(output => output)
